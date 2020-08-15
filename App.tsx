de68cc29bcefd961ca5f8cac5e180b59bdf2a710
jonsamp <sampjon@gmail.com>
2020-08-15 16:17:10 -0400

    add google play link

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,38 +7,37 @@ import {
   Dimensions,
   Animated,
   TouchableOpacity,
-} from 'react-native';
-import { Video } from 'expo-av';
-import colors from './constants/colors';
-import { height } from './constants/layout';
-import type from './constants/type';
-import AppIcon from './assets/icon.png';
-import AppStoreButton from './components/AppStoreButton';
-import Item from './components/Item';
-import PrivacyPolicy from './PrivacyPolicy';
+} from "react-native";
+import colors from "./constants/colors";
+import { height } from "./constants/layout";
+import type from "./constants/type";
+import AppIcon from "./assets/icon.png";
+import AppStoreButton from "./components/AppStoreButton";
+import Item from "./components/Item";
+import PrivacyPolicy from "./PrivacyPolicy";
 
 // feature images
-import PulsePour from './assets/pulse-pour.gif';
-import DarkMode from './assets/dark-mode.png';
-import Iced from './assets/iced.png';
-import Suggestions from './assets/suggestions.png';
+import PulsePour from "./assets/pulse-pour.gif";
+import DarkMode from "./assets/dark-mode.png";
+import Iced from "./assets/iced.png";
+import Suggestions from "./assets/suggestions.png";
 
 const MEDIUM_WIDTH = 790;
 const SMALL_WIDTH = 500;
-const twos = ['2', 'II', '✌️'];
+const twos = ["2", "II", "✌️"];
 
 export default class App extends Component {
   animatedValue = new Animated.Value(1);
 
   state = {
-    isSmallScreen: Dimensions.get('window').width < SMALL_WIDTH,
-    isMediumScreen: Dimensions.get('window').width < MEDIUM_WIDTH,
-    due: '2',
+    isSmallScreen: Dimensions.get("window").width < SMALL_WIDTH,
+    isMediumScreen: Dimensions.get("window").width < MEDIUM_WIDTH,
+    due: "2",
     privacyPolicyVisible: false,
   };
 
   componentDidMount() {
-    Dimensions.addEventListener('change', r => {
+    Dimensions.addEventListener("change", (r) => {
       if (r.window.width < SMALL_WIDTH && !this.state.isSmallScreen) {
         this.setState({ isSmallScreen: true });
       } else if (r.window.width >= SMALL_WIDTH && this.state.isSmallScreen) {
@@ -53,7 +52,7 @@ export default class App extends Component {
     });
 
     setInterval(() => {
-      const index = twos.findIndex(i => i === this.state.due);
+      const index = twos.findIndex((i) => i === this.state.due);
       let nextIndex = index + 1;
       if (nextIndex >= twos.length) {
         nextIndex = 0;
@@ -65,7 +64,7 @@ export default class App extends Component {
           duration: 400,
         }),
         {
-          start: async onComplete => {
+          start: async (onComplete) => {
             await this.setState({ due: twos[nextIndex] });
             onComplete({ finished: true });
           },
@@ -78,17 +77,17 @@ export default class App extends Component {
       ]).start();
     }, 2500);
 
-    const containsPrivacy = Boolean(window.location.href.match('privacy'));
+    const containsPrivacy = Boolean(window.location.href.match("privacy"));
     if (containsPrivacy && !this.state.privacyPolicyVisible) {
       this.setPrivacyPolicy(true);
     }
   }
 
-  setPrivacyPolicy = async value => {
+  setPrivacyPolicy = async (value) => {
     await this.setState({ privacyPolicyVisible: value });
-    const containsPrivacy = Boolean(window.location.href.match('privacy'));
+    const containsPrivacy = Boolean(window.location.href.match("privacy"));
     if (!this.state.privacyPolicyVisible && containsPrivacy) {
-      location.href = '/';
+      location.href = "/";
     }
     window.scrollTo(0, 0);
   };
@@ -106,7 +105,7 @@ export default class App extends Component {
               styles.displayHorizontal,
               {
                 minHeight: height / 3.5,
-                alignItems: 'center',
+                alignItems: "center",
               },
             ]}
           >
@@ -128,8 +127,8 @@ export default class App extends Component {
                 style={[
                   { flex: 1 },
                   isSmallScreen && {
-                    flexDirection: 'column',
-                    alignItems: 'center',
+                    flexDirection: "column",
+                    alignItems: "center",
                   },
                 ]}
               >
@@ -137,15 +136,15 @@ export default class App extends Component {
                   <Text
                     style={[
                       type.largeTitle,
-                      isSmallScreen && { textAlign: 'center' },
+                      isSmallScreen && { textAlign: "center" },
                     ]}
                   >
-                    Single Origin{' '}
+                    Single Origin{" "}
                   </Text>
                   <Animated.Text
                     style={[
                       type.largeTitle,
-                      isSmallScreen && { textAlign: 'center' },
+                      isSmallScreen && { textAlign: "center" },
                       {
                         width: 32,
                         height: 40,
@@ -168,7 +167,7 @@ export default class App extends Component {
                   style={[
                     type.body,
                     { marginTop: 8, marginBottom: 24, maxWidth: 260 },
-                    isSmallScreen && { textAlign: 'center' },
+                    isSmallScreen && { textAlign: "center" },
                   ]}
                 >
                   Learn to brew specialty coffee, now on iPhone and iPad.
@@ -177,19 +176,6 @@ export default class App extends Component {
               </View>
             </View>
           </View>
-        </View>
-        <View style={[styles.section, styles.primarySection]}>
-          <Video
-            source={{
-              uri: 'https://singleoriginapp.s3.amazonaws.com/app-preview.mp4',
-            }}
-            rate={1.0}
-            volume={1.0}
-            isMuted={false}
-            resizeMode="cover"
-            useNativeControls
-            style={{ width: 280, height: 606, backgroundColor: 'black' }}
-          />
         </View>
         <View
           style={[
@@ -209,20 +195,20 @@ export default class App extends Component {
             <View style={{ flex: 1 }}>
               <Item
                 image={PulsePour}
-                title="Pulse pouring"
-                description="Pour in steps as your coffee brews to get the perfect balance of sour, sweet, and bitter notes."
+                title='Pulse pouring'
+                description='Pour in steps as your coffee brews to get the perfect balance of sour, sweet, and bitter notes.'
                 featureImage
               />
             </View>
             <View style={{ flex: 1 }}>
               <Item
                 image={Iced}
-                title="Iced recipes"
-                description="Every pour over includes recipes for iced versions."
+                title='Iced recipes'
+                description='Every pour over includes recipes for iced versions.'
               />
               <Item
-                title="Tasting notifications"
-                description="Get a notification to taste your coffee once it has cooled. Tapping the notification takes you straight to a quick rating card."
+                title='Tasting notifications'
+                description='Get a notification to taste your coffee once it has cooled. Tapping the notification takes you straight to a quick rating card.'
               />
             </View>
           </View>
@@ -234,7 +220,7 @@ export default class App extends Component {
             ]}
           >
             <Item
-              title="Brew without a scale"
+              title='Brew without a scale'
               description="When pouring a pour over method, follow along with the pour animation on screen to get close to the correct ratios when you don't have a scale."
             />
           </View>
@@ -249,18 +235,18 @@ export default class App extends Component {
             <View style={{ flex: 1 }}>
               <Item
                 image={DarkMode}
-                title="Dark mode"
-                description="Easily brew all night with dark mode."
+                title='Dark mode'
+                description='Easily brew all night with dark mode.'
               />
               <Item
-                title="Rate your brew"
-                description="Add tasting notes, an overall rating, and notes to each brew."
+                title='Rate your brew'
+                description='Add tasting notes, an overall rating, and notes to each brew.'
               />
             </View>
             <View style={{ flex: 1 }}>
               <Item
                 image={Suggestions}
-                title="Smart suggestions"
+                title='Smart suggestions'
                 description="Once you rate your brew, you'll see a suggestion of how to improve next time."
                 featureImage
               />
@@ -276,21 +262,21 @@ export default class App extends Component {
           >
             <View style={{ flex: 1 }}>
               <Item
-                title="More recipes"
-                description="Now brewing Aeropress, Beehouse, Chemex, Chemex 3 Cup, Clever, Eva Solo, French Press, Kalita Wave (185, 155), V60 (#02, #01)"
+                title='More recipes'
+                description='Now brewing Aeropress, Beehouse, Chemex, Chemex 3 Cup, Clever, Eva Solo, French Press, Kalita Wave (185, 155), V60 (#02, #01)'
               />
               <Item
-                title="Choose your grinder"
-                description="Choose your grinder to record grind settings."
+                title='Choose your grinder'
+                description='Choose your grinder to record grind settings.'
               />
             </View>
             <View style={{ flex: 1 }}>
               <Item
-                title="More units"
-                description="Granularly define units in grams, ounces, or measuring cups. Fahrenheit and Celsius also supported."
+                title='More units'
+                description='Granularly define units in grams, ounces, or measuring cups. Fahrenheit and Celsius also supported.'
               />
               <Item
-                title="Rewritten from scratch"
+                title='Rewritten from scratch'
                 description="Single Origin 2 is a complete rebuild of Single Origin. It's faster, stronger, and pours better coffee."
               />
             </View>
@@ -304,8 +290,8 @@ export default class App extends Component {
             styles.section,
             styles.darkSection,
             {
-              flexDirection: 'row',
-              justifyContent: 'space-between',
+              flexDirection: "row",
+              justifyContent: "space-between",
               paddingHorizontal: 32,
             },
           ]}
@@ -318,7 +304,7 @@ export default class App extends Component {
               Copyright {new Date().getFullYear()}
             </Text>
           </View>
-          <View style={{ flexDirection: 'row' }}>
+          <View style={{ flexDirection: "row" }}>
             <TouchableOpacity
               style={{
                 backgroundColor: colors.dark.foreground,
@@ -347,8 +333,8 @@ const styles = StyleSheet.create({
   section: {
     minHeight: height / 3,
     paddingVertical: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     backgroundColor: colors.light.grey1,
   },
   appIcon: {
@@ -358,11 +344,11 @@ const styles = StyleSheet.create({
     borderRadius: 17,
   },
   displayHorizontal: {
-    flexDirection: 'row',
+    flexDirection: "row",
   },
   headerSmallScreen: {
-    flexDirection: 'column',
-    alignItems: 'center',
+    flexDirection: "column",
+    alignItems: "center",
   },
   darkSection: {
     backgroundColor: colors.dark.background,
