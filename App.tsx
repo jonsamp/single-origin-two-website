973481066288cdc223f8339cd45aa8c19603199f
Jon Samp <sampjon@gmail.com>
2022-05-13 01:50:27 -0400

    remove due

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,37 +7,36 @@ import {
   Dimensions,
   Animated,
   TouchableOpacity,
-} from "react-native";
-import colors from "./constants/colors";
-import { height } from "./constants/layout";
-import type from "./constants/type";
-import AppIcon from "./assets/icon.png";
-import AppStoreButton from "./components/AppStoreButton";
-import Item from "./components/Item";
-import PrivacyPolicy from "./PrivacyPolicy";
+} from 'react-native';
+import colors from './constants/colors';
+import { height } from './constants/layout';
+import type from './constants/type';
+import AppIcon from './assets/icon.png';
+import AppStoreButton from './components/AppStoreButton';
+import Item from './components/Item';
+import PrivacyPolicy from './PrivacyPolicy';
 
 // feature images
-import PulsePour from "./assets/pulse-pour.gif";
-import DarkMode from "./assets/dark-mode.png";
-import Iced from "./assets/iced.png";
-import Suggestions from "./assets/suggestions.png";
+import PulsePour from './assets/pulse-pour.gif';
+import DarkMode from './assets/dark-mode.png';
+import Iced from './assets/iced.png';
+import Suggestions from './assets/suggestions.png';
 
 const MEDIUM_WIDTH = 790;
 const SMALL_WIDTH = 500;
-const twos = ["2", "II", "✌️"];
 
 export default class App extends Component {
   animatedValue = new Animated.Value(1);
 
   state = {
-    isSmallScreen: Dimensions.get("window").width < SMALL_WIDTH,
-    isMediumScreen: Dimensions.get("window").width < MEDIUM_WIDTH,
-    due: "2",
+    isSmallScreen: Dimensions.get('window').width < SMALL_WIDTH,
+    isMediumScreen: Dimensions.get('window').width < MEDIUM_WIDTH,
+    due: '2',
     privacyPolicyVisible: false,
   };
 
   componentDidMount() {
-    Dimensions.addEventListener("change", (r) => {
+    Dimensions.addEventListener('change', (r) => {
       if (r.window.width < SMALL_WIDTH && !this.state.isSmallScreen) {
         this.setState({ isSmallScreen: true });
       } else if (r.window.width >= SMALL_WIDTH && this.state.isSmallScreen) {
@@ -51,33 +50,7 @@ export default class App extends Component {
       }
     });
 
-    setInterval(() => {
-      const index = twos.findIndex((i) => i === this.state.due);
-      let nextIndex = index + 1;
-      if (nextIndex >= twos.length) {
-        nextIndex = 0;
-      }
-
-      Animated.sequence([
-        Animated.timing(this.animatedValue, {
-          toValue: 0,
-          duration: 400,
-        }),
-        {
-          start: async (onComplete) => {
-            await this.setState({ due: twos[nextIndex] });
-            onComplete({ finished: true });
-          },
-          stop: () => {},
-        },
-        Animated.timing(this.animatedValue, {
-          toValue: 1,
-          duration: 400,
-        }),
-      ]).start();
-    }, 2500);
-
-    const containsPrivacy = Boolean(window.location.href.match("privacy"));
+    const containsPrivacy = Boolean(window.location.href.match('privacy'));
     if (containsPrivacy && !this.state.privacyPolicyVisible) {
       this.setPrivacyPolicy(true);
     }
@@ -85,9 +58,9 @@ export default class App extends Component {
 
   setPrivacyPolicy = async (value) => {
     await this.setState({ privacyPolicyVisible: value });
-    const containsPrivacy = Boolean(window.location.href.match("privacy"));
+    const containsPrivacy = Boolean(window.location.href.match('privacy'));
     if (!this.state.privacyPolicyVisible && containsPrivacy) {
-      location.href = "/";
+      location.href = '/';
     }
     window.scrollTo(0, 0);
   };
@@ -105,7 +78,7 @@ export default class App extends Component {
               styles.displayHorizontal,
               {
                 minHeight: height / 3.5,
-                alignItems: "center",
+                alignItems: 'center',
               },
             ]}
           >
@@ -127,8 +100,8 @@ export default class App extends Component {
                 style={[
                   { flex: 1 },
                   isSmallScreen && {
-                    flexDirection: "column",
-                    alignItems: "center",
+                    flexDirection: 'column',
+                    alignItems: 'center',
                   },
                 ]}
               >
@@ -136,38 +109,17 @@ export default class App extends Component {
                   <Text
                     style={[
                       type.largeTitle,
-                      isSmallScreen && { textAlign: "center" },
+                      isSmallScreen && { textAlign: 'center' },
                     ]}
                   >
-                    Single Origin{" "}
+                    Single Origin
                   </Text>
-                  <Animated.Text
-                    style={[
-                      type.largeTitle,
-                      isSmallScreen && { textAlign: "center" },
-                      {
-                        width: 32,
-                        height: 40,
-                        opacity: this.animatedValue,
-                        transform: [
-                          {
-                            scale: this.animatedValue.interpolate({
-                              inputRange: [0, 1],
-                              outputRange: [0.8, 1],
-                            }),
-                          },
-                        ],
-                      },
-                    ]}
-                  >
-                    {this.state.due}
-                  </Animated.Text>
                 </View>
                 <Text
                   style={[
                     type.body,
                     { marginTop: 8, marginBottom: 24, maxWidth: 260 },
-                    isSmallScreen && { textAlign: "center" },
+                    isSmallScreen && { textAlign: 'center' },
                   ]}
                 >
                   Learn to brew specialty coffee, now on iPhone and iPad.
@@ -290,8 +242,8 @@ export default class App extends Component {
             styles.section,
             styles.darkSection,
             {
-              flexDirection: "row",
-              justifyContent: "space-between",
+              flexDirection: 'row',
+              justifyContent: 'space-between',
               paddingHorizontal: 32,
             },
           ]}
@@ -304,7 +256,7 @@ export default class App extends Component {
               Copyright {new Date().getFullYear()}
             </Text>
           </View>
-          <View style={{ flexDirection: "row" }}>
+          <View style={{ flexDirection: 'row' }}>
             <TouchableOpacity
               style={{
                 backgroundColor: colors.dark.foreground,
@@ -333,8 +285,8 @@ const styles = StyleSheet.create({
   section: {
     minHeight: height / 3,
     paddingVertical: 60,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     backgroundColor: colors.light.grey1,
   },
   appIcon: {
@@ -344,11 +296,11 @@ const styles = StyleSheet.create({
     borderRadius: 17,
   },
   displayHorizontal: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
   headerSmallScreen: {
-    flexDirection: "column",
-    alignItems: "center",
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   darkSection: {
     backgroundColor: colors.dark.background,
